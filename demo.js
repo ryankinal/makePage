aaa1900ae05edd4f30bd866e596f9aa61d636852
Ryan Kinal <ryan.kinal@gmail.com>
2011-11-04 03:58:54 -0400

    initial commit

---
+++ demo.js
@@ -0,0 +1,59 @@
+$(function() {
+    var content = {
+        'index.html': $('<div>').makePage({
+                subsubtitle: 'Download It!',
+                content: $.makePage.list([
+                    $.makePage.link('jquery.makepage.js', 'Unminified source'),
+                    $.makePage.link('jquery.makepage.js', 'Minified source')
+                ])
+            }).makePage({
+                subsubtitle: 'Include It!',
+                content: $.makePage.paragraph('&lt;script type="text/javascript" src="jquery.makepage.min.js"&gt;&lt;/script&gt;', 'code')
+            }).makePage({
+                subsubtitle: 'Use It!',
+                content: $.makePage.paragraph('$("body").makePage({title: "jQuery.makePage", nav: [{url: "index.html", text: "Home"}, {url: "about.html", text: "About"}], content: "Check out my sweet new page I made with makePage!", footer: "Copyright me, Inc. 2011"});', 'code')
+            }),
+        'options.html': $('<div>').makePage({
+                subsubtitle: 'Options',
+                content: [
+                    $.makePage.paragraph('There aren\'t actually that many options for makePage. Here\'s the full list'),
+                    $.makePage.list([
+                        '<strong>title</strong> - the text of the h1 tag for this page',
+                        '<strong>subtitle</strong> - the text of the h2 tag for this page',
+                        '<strong>subsubtitle</strong> - the text of the h3 tag for this page',
+                        '<strong>nav</strong> - an array of objects, each of which has a <em>url</em>, a <em>text</em>, and (optionally) a <em>blank</em>, each of which represents a navigation items',
+                        '<strong>content</strong> - any item that can be passed to <em>jQuery.append</em> or an array of such items, these represent the main content of the page',
+                        '<strong>footer</strong> - any item that can be passed to <em>jQuery.append</em> and represents the footer of the page'
+                    ]);
+                ]
+            }).makePage({
+                subsubtitle: 'Utitlities',
+                content: [
+                    $.makePage.paragraph('makePage includes a few utility functions, so you don\'t have to deal with all that pesky HTML'),
+                    $.makePage.list([
+                        '<strong>link(url, text, blank)</strong> - Creates an anchor tag with href=<em>url</em>, a text value of <em>text</em> and, if <em>blank</em> is true, target=&quot;_blank&quot;',
+                        '<strong>list(items, klass)</strong> - Creates an unordered list out of the items in array <em>items</em>, and the list optionally has className = <em>klass</em>',
+                        '<strong>paragraph(content, klass)</strong> - Creates a paragraph with the given content appended to it, and (optionally) <em>klass</em> as a <em>className</em>'                        
+                    ]);
+                ]
+            });
+    };
+    
+    var href = window.location.href,
+        parts = href.split('/'),
+        page = parts.pop().toLowerCase();
+    
+    $('#container').makePage({
+        title: 'jQuery.makePage',
+        subtitle: 'We don\'t need no stinkin\' HTML!',
+        nav: [
+            {url: 'index.html', text: 'Home'},
+            {url: 'options.html', text: 'Options'},
+            {url: 'nohtml.html', text: 'NoHTML'},
+            {url: 'http://jquery.com', text: 'jQuery', blank: true},
+            {url: 'http://cowbell.js', text: 'Cowbell', blank: true}
+        ],
+        content: content[page],
+        footer: 'Use this plugin for whatever you want, however you want. But don\'t use this plugin.'
+    });
+});
---
+++ jquery.makepage.js
@@ -0,0 +1,146 @@
+/*
+    makePage: Removes all of the work from doing complicated HTML coding.
+    
+    options:
+        title: this text gets put in the header element, in an H1
+        subtitle: this text gets put in the header element in an H2 tag
+        subsubtitle: this text gets put in the header element in an H3 tag
+        nav: an array of objects, each of which has a "url" and a "text" property
+        content: anything that could be passed to jQuery.append, or an array of
+            things that could be passed to jQuery.append
+        footer: content in the footer
+*/
+(function($) {
+    $.makePage = {
+        list: function(items, klass)
+        {
+            var i,
+                list = $('<ul>'),
+                item;
+            
+            for (i = 0; i < items.length; i++)
+            {
+                item = $('<li />');
+                item.append(items[i]);
+                list.append(item);
+            }
+            
+            if (klass)
+            {
+                list.addClass(klass);
+            }
+            
+            return list;
+        },
+        paragraph: function(content, klass)
+        {
+            var $p = $('<p />');
+            $p.append(content);
+            if (klass)
+            {
+                $p.addClass(klass);
+            }
+            
+            return $p;
+        },
+        link: function(url, text, blank)
+        {
+            var $a = $('<a />', {href: url});
+            $a.append(text);
+            
+            if (blank)
+            {
+                $a.attr('target', '_blank');
+            }
+            
+            return $a;
+        }
+    };
+    
+    $.fn.makePage = function(args)
+    {
+        if (args && typeof args === 'object')
+        {
+            var $container = $('<div>', {'class': 'container'}),
+                $header,
+                $h1,
+                $h2,
+                $h3,
+                $left,
+                $nav,
+                $right = $('<div>', {'class': 'second'}),
+                $footer,
+                $li,
+                i;
+                
+            if (args.title || args.subtitle || args.subsubtitle)
+            {
+                $header = $('<div>', {'class': 'header'});
+                
+                if (args.title)
+                {
+                    $h1 = $('<h1>' + args.title + '</h1>');
+                    $header.append($h1);
+                }
+                
+                if (args.subtitle)
+                {
+                    $h2 = $('<h2>' + args.subtitle + '</h1>');
+                    $header.append($h2);
+                }
+                
+                if (args.subsubtitle)
+                {
+                    $h3 = $('<h3>' + args.subsubtitle + '</h3>');
+                    $header.append($h3);
+                }
+                
+                $container.append($header);
+            }
+            
+            if (args.nav && args.nav.length)
+            {
+                $left = $('<div>', {'class': 'first'});
+                $nav = $('<ul>', {'class': 'nav'});
+                
+                for (i = 0; i < args.nav.length; i++)
+                {
+                    $li = $('<li />');
+                    $li.append($.makePage.link(args.nav[i].url, args.nav[i].text, args.nav[i].blank));
+                    $nav.append($li);
+                }
+                
+                $left.append($nav);
+                $container.append($left);
+            }
+            
+            if (args.content)
+            {
+                if (args.content.length)
+                {
+                    for (i = 0; i < args.content.length; i++)
+                    {
+                        $right.append(args.content[i]);
+                    }
+                }
+                else
+                {
+                    $right.append(args.content);
+                }
+            }
+            
+            $container.append($right);
+            
+            if (args.footer)
+            {
+                $footer = $('<div>', {'class': 'footer'});
+                $footer.append(args.footer);
+                $container.append($footer);
+            }
+            
+            this.append($container);
+        }
+        
+        return $(this);
+    };
+})(jQuery);
